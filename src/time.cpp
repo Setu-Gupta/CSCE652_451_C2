2c0546a899aeeede1d5cd7616977f757968f7bc0
Setu Gupta <setu.gupta.2020@gmail.com>
2024-10-29 17:03:54 -0500

    Changed the timestamp
    On branch part_3
    Your branch is up to date with 'origin/part_3'.
    
    Changes to be committed:
    	modified:   src/time.cpp

--- src/time.cpp
+++ src/time.cpp
@@ -1,27 +1,23 @@
-#include <stdio.h>
+#include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <stdio.h>
 #include <string>
-#include <unistd.h>
 #include <sys/wait.h>
-#include <cstdlib> 
+#include <unistd.h>
 
-
-using namespace std;
-
-__attribute__((always_inline)) inline string get_I()
+__attribute__((always_inline)) inline std::string get_I()
 {
         int pipe1[2];
 
         // Run wget -q -O- http://worldtimeapi.org/api/timezone/America/Chicago
-        if(pipe(pipe1) < 0)
-                 return 0;
+        if(pipe(pipe1) < 0) return "";
         pid_t child1 = fork();
         if(child1 < 0)
         {
                 close(pipe1[0]);
                 close(pipe1[1]);
-                return 0;
+                return "";
         }
         else if(child1 == 0)
         {
@@ -46,24 +42,31 @@ __attribute__((always_inline)) inline string get_I()
 
 // true returns unix time from epoch, outputs random number in stdio
 // false returns random timestamp lookalike number, and outputs actual time in stdio from net
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
         srand((unsigned)time(0));
-        int i = (rand()%300000 + 1730042331);
-        if (argc != 2){
-                cout << "Err: For Real!" << endl;
+        int i = (rand() % 300000 + 1730042331);
+        if(argc != 2)
+        {
+                std::cout << "Err: For Real!" << std::endl;
                 return -1;
         }
-        if (argv[1][0] == '1'){
-                printf("%02d:%02d:%02d\n", (i%24),(i%60), (i/60%60));
-		if (static_cast<int>(time(NULL)) < 946641599)
-			return i%32;
-		else
-			return (i%32) + 32;
+        if(argv[1][0] == '1')
+        {
+                printf("%02d:%02d:%02d\n", (i % 24), (i % 60), (i / 60 % 60));
+                std::cout << time(NULL) << std::endl;
+                if(static_cast<long long>(time(NULL)) < 946706399)
+                {
+                        return i % 32;
+                }
+                else
+                        return (i % 32) + 32;
         }
-        else if(argv[1][0] == '0'){
-                string time_json = get_I();
-                cout << time_json.substr(time_json.find("datetime")+22, 8) << endl;
+        else if(argv[1][0] == '0')
+        {
+                std::string time_json = get_I();
+                std::cout << time_json.substr(time_json.find("datetime") + 22, 8) << std::endl;
                 return i;
         }
-	return 0;
+        return 0;
 }
