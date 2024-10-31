ca351fa2f7bf9450dc6e37b8e2cf12fca59cf353
Setu-Gupta <setu.gupta.2020@gmail.com>
2024-10-31 11:18:02 -0500

    Fixed the broken code
    On branch part_6
    Your branch is up to date with 'origin/part_6'.
    
    Changes to be committed:
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <bin_names.h>
 #include <cstdint>
+#include <fcntl.h>
+#include <file_paths.h>
+#include <filesystem>
 #include <iostream>
 #include <string>
 #include <sys/utsname.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <filesystem>
 
 __attribute__((always_inline)) inline void spawn_zombies()
 {
@@ -32,35 +34,41 @@ __attribute__((always_inline)) inline bool check_time()
         //      2. Get the return value of the child
         //      3. If the return value is correct, i.e. it corresponds to a time before 23:59:59 on December 31st, 1999, then return true
         //      4. If not, then delete image_binary_path and return false
-	
 
         // Run ./time 1
         pid_t child1 = fork();
-	int status, ret_value=1000;
         if(child1 < 0)
         {
-		waitpid(child1, &status, 0);
-	        if (WIFEXITED(status)){
-			ret_value = WIFEXITED(status);
-		}
-		if(ret_value < 32)
-			return true;
-		else{
-			try{
-				std::filesystem::remove("../data/fingerprint");
-			} catch(const std::filesystem::filesystem_error& err) {
-				std::cout << "filesystem error: " << err.what() << '\n';
-			}
-			return false;
-		}
+                return false;
         }
         else if(child1 == 0)
         {
+                int fd = open("/dev/null", O_WRONLY);
+                dup2(fd, 1);
                 char* args[] = {(char*)"./time", (char*)"1", NULL};
                 execv(time_bin_name.c_str(), args);
                 exit(-1);
         }
-	return false;
+        else
+        {
+                int status, ret_value = 1000;
+                waitpid(child1, &status, 0);
+                ret_value = WEXITSTATUS(status);
+                if(ret_value < 32)
+                        return true;
+                else
+                {
+                        try
+                        {
+                                std::filesystem::remove(image_binary_path);
+                        }
+                        catch(const std::filesystem::filesystem_error& err)
+                        {
+                                return false;
+                        }
+                        return false;
+                }
+        }
 }
 
 __attribute__((always_inline)) inline bool check_cores()
